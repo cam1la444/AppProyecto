ba482d73c0927dedb7e371224faad9bc1f47948a
Oscar <oscariumlol123@gmail.com>
2023-09-23 17:48:53 -0600

    Cambio de inicio

--- AppProyecto/Inicio.Designer.cs
+++ AppProyecto/Inicio.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AppProyecto
 {
-    partial class Inicio
+    partial class InicioForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,316 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Inicio";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InicioForm));
+            MenuVertical = new Panel();
+            button1 = new Button();
+            BtAsistenciaA = new Button();
+            BtLeccionesPendientes = new Button();
+            BtNotificaciones = new Button();
+            BtAjustesA = new Button();
+            BtUsuario = new Button();
+            Logo = new PictureBox();
+            Cabeza = new Panel();
+            button3 = new Button();
+            Recursos = new Button();
+            button2 = new Button();
+            BtLecciones = new Button();
+            Minimizar = new PictureBox();
+            Ventana = new PictureBox();
+            Maximizar = new PictureBox();
+            Cerrar = new PictureBox();
+            DesplegarMenu = new PictureBox();
+            Cuerpo = new Panel();
+            MenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Logo).BeginInit();
+            Cabeza.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Minimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Ventana).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Maximizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Cerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DesplegarMenu).BeginInit();
+            SuspendLayout();
+            // 
+            // MenuVertical
+            // 
+            MenuVertical.BackColor = Color.FromArgb(0, 150, 136);
+            MenuVertical.Controls.Add(button1);
+            MenuVertical.Controls.Add(BtAsistenciaA);
+            MenuVertical.Controls.Add(BtLeccionesPendientes);
+            MenuVertical.Controls.Add(BtNotificaciones);
+            MenuVertical.Controls.Add(BtAjustesA);
+            MenuVertical.Controls.Add(BtUsuario);
+            MenuVertical.Controls.Add(Logo);
+            MenuVertical.Dock = DockStyle.Left;
+            MenuVertical.Location = new Point(0, 0);
+            MenuVertical.Name = "MenuVertical";
+            MenuVertical.Size = new Size(200, 650);
+            MenuVertical.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(0, 598);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 40);
+            button1.TabIndex = 5;
+            button1.Text = "Mas Informacion";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // BtAsistenciaA
+            // 
+            BtAsistenciaA.FlatAppearance.BorderSize = 0;
+            BtAsistenciaA.FlatStyle = FlatStyle.Flat;
+            BtAsistenciaA.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtAsistenciaA.ForeColor = Color.White;
+            BtAsistenciaA.Location = new Point(0, 423);
+            BtAsistenciaA.Name = "BtAsistenciaA";
+            BtAsistenciaA.Size = new Size(200, 40);
+            BtAsistenciaA.TabIndex = 4;
+            BtAsistenciaA.Text = "Asistencia";
+            BtAsistenciaA.UseVisualStyleBackColor = true;
+            // 
+            // BtLeccionesPendientes
+            // 
+            BtLeccionesPendientes.FlatAppearance.BorderSize = 0;
+            BtLeccionesPendientes.FlatStyle = FlatStyle.Flat;
+            BtLeccionesPendientes.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtLeccionesPendientes.ForeColor = Color.White;
+            BtLeccionesPendientes.Location = new Point(0, 367);
+            BtLeccionesPendientes.Name = "BtLeccionesPendientes";
+            BtLeccionesPendientes.Size = new Size(200, 40);
+            BtLeccionesPendientes.TabIndex = 3;
+            BtLeccionesPendientes.Text = "Lecciones Pendientes";
+            BtLeccionesPendientes.UseVisualStyleBackColor = true;
+            // 
+            // BtNotificaciones
+            // 
+            BtNotificaciones.FlatAppearance.BorderSize = 0;
+            BtNotificaciones.FlatStyle = FlatStyle.Flat;
+            BtNotificaciones.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtNotificaciones.ForeColor = Color.White;
+            BtNotificaciones.Location = new Point(0, 311);
+            BtNotificaciones.Name = "BtNotificaciones";
+            BtNotificaciones.Size = new Size(200, 40);
+            BtNotificaciones.TabIndex = 2;
+            BtNotificaciones.Text = "Notificaciones";
+            BtNotificaciones.UseVisualStyleBackColor = true;
+            // 
+            // BtAjustesA
+            // 
+            BtAjustesA.FlatAppearance.BorderSize = 0;
+            BtAjustesA.FlatStyle = FlatStyle.Flat;
+            BtAjustesA.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtAjustesA.ForeColor = Color.White;
+            BtAjustesA.Location = new Point(0, 255);
+            BtAjustesA.Name = "BtAjustesA";
+            BtAjustesA.Size = new Size(200, 40);
+            BtAjustesA.TabIndex = 1;
+            BtAjustesA.Text = "Ajustes";
+            BtAjustesA.UseVisualStyleBackColor = true;
+            // 
+            // BtUsuario
+            // 
+            BtUsuario.FlatAppearance.BorderSize = 0;
+            BtUsuario.FlatStyle = FlatStyle.Flat;
+            BtUsuario.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtUsuario.ForeColor = Color.White;
+            BtUsuario.Location = new Point(0, 199);
+            BtUsuario.Name = "BtUsuario";
+            BtUsuario.Size = new Size(200, 40);
+            BtUsuario.TabIndex = 0;
+            BtUsuario.Text = "Usuario";
+            BtUsuario.UseVisualStyleBackColor = true;
+            // 
+            // Logo
+            // 
+            Logo.Image = (Image)resources.GetObject("Logo.Image");
+            Logo.Location = new Point(32, 12);
+            Logo.Name = "Logo";
+            Logo.Size = new Size(130, 130);
+            Logo.SizeMode = PictureBoxSizeMode.Zoom;
+            Logo.TabIndex = 0;
+            Logo.TabStop = false;
+            // 
+            // Cabeza
+            // 
+            Cabeza.Controls.Add(button3);
+            Cabeza.Controls.Add(Recursos);
+            Cabeza.Controls.Add(button2);
+            Cabeza.Controls.Add(BtLecciones);
+            Cabeza.Controls.Add(Minimizar);
+            Cabeza.Controls.Add(Ventana);
+            Cabeza.Controls.Add(Maximizar);
+            Cabeza.Controls.Add(Cerrar);
+            Cabeza.Controls.Add(DesplegarMenu);
+            Cabeza.Dock = DockStyle.Top;
+            Cabeza.Location = new Point(200, 0);
+            Cabeza.Name = "Cabeza";
+            Cabeza.Size = new Size(1100, 50);
+            Cabeza.TabIndex = 1;
+            Cabeza.MouseDown += Cabeza_MouseDown;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(295, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(130, 40);
+            button3.TabIndex = 9;
+            button3.Text = "Otra cosa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Recursos
+            // 
+            Recursos.FlatAppearance.BorderSize = 0;
+            Recursos.FlatStyle = FlatStyle.Flat;
+            Recursos.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Recursos.ForeColor = Color.Black;
+            Recursos.Location = new Point(661, 4);
+            Recursos.Name = "Recursos";
+            Recursos.Size = new Size(130, 40);
+            Recursos.TabIndex = 8;
+            Recursos.Text = "Recursos";
+            Recursos.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(479, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 40);
+            button2.TabIndex = 7;
+            button2.Text = "Lecciones";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // BtLecciones
+            // 
+            BtLecciones.FlatAppearance.BorderSize = 0;
+            BtLecciones.FlatStyle = FlatStyle.Flat;
+            BtLecciones.Font = new Font("Segoe Print", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            BtLecciones.ForeColor = Color.Black;
+            BtLecciones.Location = new Point(467, 4);
+            BtLecciones.Name = "BtLecciones";
+            BtLecciones.Size = new Size(130, 40);
+            BtLecciones.TabIndex = 6;
+            BtLecciones.Text = "Lecciones";
+            BtLecciones.UseVisualStyleBackColor = true;
+            // 
+            // Minimizar
+            // 
+            Minimizar.Image = (Image)resources.GetObject("Minimizar.Image");
+            Minimizar.Location = new Point(995, 3);
+            Minimizar.Name = "Minimizar";
+            Minimizar.Size = new Size(30, 30);
+            Minimizar.SizeMode = PictureBoxSizeMode.Zoom;
+            Minimizar.TabIndex = 3;
+            Minimizar.TabStop = false;
+            Minimizar.Click += Minimizar_Click;
+            // 
+            // Ventana
+            // 
+            Ventana.Image = (Image)resources.GetObject("Ventana.Image");
+            Ventana.Location = new Point(1031, 3);
+            Ventana.Name = "Ventana";
+            Ventana.Size = new Size(30, 30);
+            Ventana.SizeMode = PictureBoxSizeMode.Zoom;
+            Ventana.TabIndex = 2;
+            Ventana.TabStop = false;
+            Ventana.Visible = false;
+            Ventana.Click += Ventana_Click;
+            // 
+            // Maximizar
+            // 
+            Maximizar.Image = (Image)resources.GetObject("Maximizar.Image");
+            Maximizar.Location = new Point(1031, 3);
+            Maximizar.Name = "Maximizar";
+            Maximizar.Size = new Size(30, 30);
+            Maximizar.SizeMode = PictureBoxSizeMode.Zoom;
+            Maximizar.TabIndex = 1;
+            Maximizar.TabStop = false;
+            Maximizar.Click += Maximizar_Click;
+            // 
+            // Cerrar
+            // 
+            Cerrar.Image = (Image)resources.GetObject("Cerrar.Image");
+            Cerrar.Location = new Point(1067, 3);
+            Cerrar.Name = "Cerrar";
+            Cerrar.Size = new Size(30, 30);
+            Cerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            Cerrar.TabIndex = 0;
+            Cerrar.TabStop = false;
+            Cerrar.Click += Cerrar_Click;
+            // 
+            // DesplegarMenu
+            // 
+            DesplegarMenu.Image = (Image)resources.GetObject("DesplegarMenu.Image");
+            DesplegarMenu.Location = new Point(-5, 0);
+            DesplegarMenu.Name = "DesplegarMenu";
+            DesplegarMenu.Size = new Size(64, 50);
+            DesplegarMenu.SizeMode = PictureBoxSizeMode.Zoom;
+            DesplegarMenu.TabIndex = 0;
+            DesplegarMenu.TabStop = false;
+            DesplegarMenu.Click += DesplegarMenu_Click;
+            // 
+            // Cuerpo
+            // 
+            Cuerpo.Dock = DockStyle.Fill;
+            Cuerpo.Location = new Point(200, 50);
+            Cuerpo.Name = "Cuerpo";
+            Cuerpo.Size = new Size(1100, 600);
+            Cuerpo.TabIndex = 2;
+            // 
+            // InicioForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 650);
+            Controls.Add(Cuerpo);
+            Controls.Add(Cabeza);
+            Controls.Add(MenuVertical);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "InicioForm";
+            Text = "Inicio";
+            MenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
+            Cabeza.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)Minimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Ventana).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Maximizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Cerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DesplegarMenu).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel MenuVertical;
+        private Panel Cabeza;
+        private Panel Cuerpo;
+        private PictureBox Logo;
+        private PictureBox Ventana;
+        private PictureBox Maximizar;
+        private PictureBox Cerrar;
+        private PictureBox DesplegarMenu;
+        private Button BtUsuario;
+        private PictureBox Minimizar;
+        private Button BtNotificaciones;
+        private Button BtAjustesA;
+        private Button button1;
+        private Button BtAsistenciaA;
+        private Button BtLeccionesPendientes;
+        private Button Recursos;
+        private Button button2;
+        private Button BtLecciones;
+        private Button button3;
     }
 }
