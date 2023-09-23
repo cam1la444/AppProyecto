dc2b3dfc0ada3146159b7f5fea5fb13614be2109
cam1la444 <camila24melara@gmail.com>
2023-09-22 21:04:37 -0600

    Nombre formulario

--- AppProyecto/Form1.Designer.cs
+++ AppProyecto/Form1.Designer.cs
@@ -28,57 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            button1 = new Button();
-            button2 = new Button();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(285, 141);
-            label1.Name = "label1";
-            label1.Size = new Size(50, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Poyecto";
-            // 
-            // button1
-            // 
-            button1.Location = new Point(231, 270);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(189, 194);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 2;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(829, 523);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(label1);
             MaximizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Lecciones";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
-        private Button button1;
-        private Button button2;
     }
 }
