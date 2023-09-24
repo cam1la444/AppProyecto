326533a0fabe8b18acfe596f17d61f30b591b05b
cam1la444 <camila24melara@gmail.com>
2023-09-23 19:03:03 -0600

    btn

--- AppProyecto/Lecciones.Designer.cs
+++ AppProyecto/Lecciones.Designer.cs
@@ -30,8 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CuerpoLecciones));
             panel1 = new Panel();
-            LeccionComida = new Label();
             pictureBox1 = new PictureBox();
+            LeccionComida = new Label();
+            button1 = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -40,38 +41,51 @@
             // 
             panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(LeccionComida);
-            panel1.Location = new Point(12, 12);
+            panel1.Location = new Point(10, 9);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(251, 98);
+            panel1.Size = new Size(220, 74);
             panel1.TabIndex = 0;
-            // 
-            // LeccionComida
-            // 
-            LeccionComida.AutoSize = true;
-            LeccionComida.Font = new Font("Segoe Print", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            LeccionComida.Location = new Point(138, 33);
-            LeccionComida.Name = "LeccionComida";
-            LeccionComida.Size = new Size(78, 30);
-            LeccionComida.TabIndex = 1;
-            LeccionComida.Text = "Comida";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(107, 98);
+            pictureBox1.Size = new Size(94, 74);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
+            // LeccionComida
+            // 
+            LeccionComida.AutoSize = true;
+            LeccionComida.Font = new Font("Segoe Print", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            LeccionComida.Location = new Point(121, 25);
+            LeccionComida.Name = "LeccionComida";
+            LeccionComida.Size = new Size(63, 24);
+            LeccionComida.TabIndex = 1;
+            LeccionComida.Text = "Comida";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(404, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // CuerpoLecciones
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "CuerpoLecciones";
             Text = "Lecciones";
             panel1.ResumeLayout(false);
@@ -85,5 +99,6 @@
         private Panel panel1;
         private Label LeccionComida;
         private PictureBox pictureBox1;
+        private Button button1;
     }
 }
