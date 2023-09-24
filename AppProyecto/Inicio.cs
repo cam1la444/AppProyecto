0087acf9c1a0b89ba70e7d8e97945a39bc088a87
cam1la444 <camila24melara@gmail.com>
2023-09-23 20:47:30 -0600

    Revert "Cambio de inicio"

--- AppProyecto/Inicio.cs
+++ AppProyecto/Inicio.cs
@@ -12,56 +12,9 @@ namespace AppProyecto
 {
     public partial class Inicio : Form
     {
-        public InicioForm()
+        public Inicio()
         {
             InitializeComponent();
-        }
-
-        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
-        private extern static void ReleaseCapture();
-        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
-        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
-
-        private void DesplegarMenu_Click(object sender, EventArgs e)
-        {
-            if (MenuVertical.Width == 150)
-            {
-                MenuVertical.Width = 0;
-            }
-            else
-            {
-                MenuVertical.Width = 150;
-            }
-        }
-
-        private void Cerrar_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void Maximizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Maximized;
-            Maximizar.Visible = false;
-            Ventana.Visible = true;
-        }
-
-        private void Ventana_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Normal;
-            Maximizar.Visible = true;
-            Ventana.Visible = false;
-        }
-
-        private void Minimizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void Cabeza_MouseDown(object sender, MouseEventArgs e)
-        {
-            ReleaseCapture();
-            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
